9205f866f4a05b359912288ac819222599fd0141
Anubhav <anubhavchawla02@gmail.com>
2022-10-25 23:03:52 +0530

    local commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 function App() {
+  const successCallback = (position) => {
+    console.log(position);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   return (
     <>
       <Navbar/>
